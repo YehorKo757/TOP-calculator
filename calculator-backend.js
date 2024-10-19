bc73c12aa837740a54f7407106281a86d2ee6731
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-19 11:58:53 +0100

    Add initial files. Add 4 basic functions: addition, subtraction, multiplication, division

---
+++ calculator-backend.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+      return a + b;
+}
+
+function subtract(a, b) {
+      return a - b;
+}
+
+function multiply(a, b) {
+      return a * b;
+}
+
+function divide(a, b) {
+      return a / b;
+}
+
+let a = 10;
+let b = 5;
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
