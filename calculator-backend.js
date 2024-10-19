1161bbe51411fb8c8e04ff6c59404acec3404b26
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-19 19:28:53 +0100

    Midpoint in on functionality of operation buttons

--- calculator-backend.js
+++ calculator-backend.js
@@ -1,38 +1,48 @@
 function add(a, b) {
-      return a + b;
+      return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-      return a - b;
+      return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-      return a * b;
+      return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-      return a / b;
+      if (b !== "0") {
+            return parseFloat(a) / parseFloat(b);
+      } else {
+            return "ZeroDivision error!"
+      }
+      
 }
 
-let firstNumber = undefined;
-let secondNumber = undefined;
-let operator = undefined;
-let result = undefined;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let result = "";
 let display = 0;
+let check = false;
 
 function operate(operator, firstNumber, secondNumber) {
       switch(operator) {
-            case "plus":
+            case "add":
                   result = add(firstNumber, secondNumber);
+                  return result;
                   break;
-            case "minus":
+            case "subtract":
                   result = subtract(firstNumber, secondNumber);
+                  return result;
                   break;
             case "multiply":
                   result = multiply(firstNumber, secondNumber);
+                  return result;
                   break;
             case "divide":
                   result = divide(firstNumber, secondNumber);
+                  return result;
                   break;
       }
 }
@@ -46,17 +56,83 @@ function populateDisplay(buttonValue){
                   display = buttonValue;
                   displayItem.textContent = display;
             } else {
-                  display = displayItem.textContent.concat(buttonValue);
-                  displayItem.textContent = display;
+                  displayItem.textContent = displayItem.textContent.concat(buttonValue);
+                  display = buttonValue;
             }
       }
-      
+}
+
+function clearDisplay() {
+      display = "";
+      displayItem.textContent = display;
+}
+
+function operatorToSign(operator) {
+      switch (operator) {
+            case "add":
+                  return "+";
+                  brake;
+            case "subtract":
+                  return "-";
+                  brake;
+            case "multiply":
+                  return "*";
+                  brake;
+            case "divide":
+                  return "/";
+                  brake;
+      }
 }
 
 let buttons = document.querySelector(".buttons");
 buttons.addEventListener("click", (event) => {
+
       if (event.target.parentElement.classList.contains("number")) {
             let buttonValue = event.target.parentElement.id;
-            populateDisplay(buttonValue);
+            if (check == false) {
+                  populateDisplay(buttonValue);
+            } else {
+                  clearDisplay();
+                  check = false;
+                  populateDisplay(buttonValue);
+            }
+            
+      } 
+      // This one to make "back" button work when clicking on image as well. Image is child of button, that is child of div with target class.
+      else if (event.target.parentElement.id == "back" || event.target.parentElement.parentElement.id == "back") {
+            display = display.slice(0, -1);
+            displayItem.textContent = display;
+      } 
+      
+      else if (event.target.parentElement.classList.contains("operation") && event.target.parentElement.id !== 'equal') {
+            if (firstNumber == "") {
+                  firstNumber = display;
+                  operator = event.target.parentElement.id;
+                  displayItem.textContent = display + operatorToSign(operator);
+                  console.log("event: 1");
+                  console.log(display);
+                  console.log(firstNumber);
+                  console.log(secondNumber);
+                  console.log(operator);
+                  console.log(check);
+            } else {
+                  secondNumber = display;
+                  check = true;
+                  console.log("event: 2a");
+                  console.log(display);
+                  console.log(firstNumber);
+                  console.log(secondNumber);
+                  console.log(operator);   
+                  firstNumber = operate(operator, firstNumber, secondNumber);
+                  display = firstNumber;
+                  secondNumber = "";
+                  operator = event.target.parentElement.id;
+                  displayItem.textContent = display + operatorToSign(operator);
+                  console.log("event: 2b");
+                  console.log(display);
+                  console.log(firstNumber);
+                  console.log(secondNumber);
+                  console.log(operator);   
+                  }                     
       }
 })
