d6f84d8afd421447f2a9d4ddc5dcca3d54680dda
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-19 16:27:14 +0100

    Add functionality to populate display, when number buttons are pressed

--- calculator-backend.js
+++ calculator-backend.js
@@ -18,6 +18,7 @@ let firstNumber = undefined;
 let secondNumber = undefined;
 let operator = undefined;
 let result = undefined;
+let display = 0;
 
 function operate(operator, firstNumber, secondNumber) {
       switch(operator) {
@@ -35,3 +36,27 @@ function operate(operator, firstNumber, secondNumber) {
                   break;
       }
 }
+
+let displayItem = document.querySelector(".display");
+displayItem.textContent = display;
+
+function populateDisplay(buttonValue){
+      if (displayItem.textContent.length < 9) {
+            if (display == "0") {
+                  display = buttonValue;
+                  displayItem.textContent = display;
+            } else {
+                  display = displayItem.textContent.concat(buttonValue);
+                  displayItem.textContent = display;
+            }
+      }
+      
+}
+
+let buttons = document.querySelector(".buttons");
+buttons.addEventListener("click", (event) => {
+      if (event.target.parentElement.classList.contains("number")) {
+            let buttonValue = event.target.parentElement.id;
+            populateDisplay(buttonValue);
+      }
+})
